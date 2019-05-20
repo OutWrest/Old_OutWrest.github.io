5e8135cf475be6ac6b839a6814bd7eaf50817296
Salah Abbas <724336@c02v62e3gfwm.nkcschools.org>
2019-05-20 13:45:53 -0500

    Ayo

---
+++ Pac_the_Man/ghost.js
@@ -0,0 +1,136 @@
+class ghost {
+  constructor(hi,wd,maze,type_ghost) {
+    this.map = maze;
+    this.type = type_ghost;
+    for(var i=0; i<this.map.length; i++) {
+        for(var x=0; x<this.map[i].length; x++) {
+          if(this.map[i][x]==this.type) {
+            this.xc = x;
+            this.yc = i;
+          }
+        }
+    }
+    this.h = hi;
+    this.w = wd;
+    this.x = this.xc * this.h/20;
+    this.y = this.yc * this.w/20;
+    this.dir = 5;
+    this.speed = 2;
+    this.blocked = false;
+    
+  }
+  
+  draw_ght() {
+    if(this.type == 'i') {
+      fill(0,204,204);
+      ellipseMode(CENTER);
+      ellipse(this.x+this.w/40, this.y+this.h/40, this.w/20, this.h/20);
+      rectMode(CENTER);
+      rect(this.x+this.w/40,this.y+this.h/20 -this.h/80, this.w/20, this.h/40);
+    }
+    if(this.type == 'p') {
+      fill(204,0,102);
+      ellipseMode(CENTER);
+      ellipse(this.x+this.w/40, this.y+this.h/40, this.w/20, this.h/20);
+      rectMode(CENTER);
+      rect(this.x+this.w/40,this.y+this.h/20 -this.h/80, this.w/20, this.h/40);
+    }
+    if(this.type == 'b') {
+      fill(255,0,0);
+      ellipseMode(CENTER);
+      ellipse(this.x+this.w/40, this.y+this.h/40, this.w/20, this.h/20);
+      rectMode(CENTER);
+      rect(this.x+this.w/40,this.y+this.h/20 -this.h/80, this.w/20, this.h/40);
+    }
+    if(this.type == 'c') {
+      fill(255,198,0);
+      ellipseMode(CENTER);
+      ellipse(this.x+this.w/40, this.y+this.h/40, this.w/20, this.h/20);
+      rectMode(CENTER);
+      rect(this.x+this.w/40,this.y+this.h/20 -this.h/80, this.w/20, this.h/40);
+    }
+    fill(255);
+    triangle(this.x,this.y+this.h/20,this.x+this.w/80, this.y+this.h/20-this.h/80, this.x+this.w/40, this.y+this.h/20);
+    triangle(this.x+this.w/40, this.y+this.h/20, this.x+this.w/40+this.w/80, this.y+this.h/20-this.h/80, this.x+this.w/20, this.y+this.h/20);
+    circle(this.x+this.w/80,this.y+this.h/20-this.h/30,this.h/160);
+    circle(this.x+this.w/40+this.w/80,this.y+this.h/20-this.h/30,this.h/160);
+    fill(0);
+    circle(this.x+this.w/80,this.y+this.h/20-this.h/30,this.h/320);
+    circle(this.x+this.w/40+this.w/80,this.y+this.h/20-this.h/30,this.h/320);
+  }
+  
+  move() {//0 right 1 left 2 up 3 down
+    if((((this.y+(this.h/40)) % (this.h/20)) == 20) && ((((this.x+(this.w/40)) % (this.w/20)) == 20))) {
+      var pos = updatePos(this.x,this.y,this.h,this.w);
+      if(this.xc != pos[0] || this.yc != pos[1]) {
+        for(var i=0; i<this.map.length; i++) {
+          for(var x=0; x<this.map[i].length; x++) {
+            if(this.map[i][x]==this.type) {
+              this.map[i][x] = 0;
+            }
+          }
+        }
+        this.xc = pos[0];
+        this.yc = pos[1];
+        for(var i=0; i<this.map.length; i++) {
+          for(var x=0; x<this.map[i].length; x++) {
+            if(i == this.yc && x == this.xc) {
+              this.map[i][x] = this.type;
+            }
+          }
+        }
+      }
+      //randMove(this,this.xc,this.yc);
+      g_play(this,this.xc,this.yc, this.x,this.y);
+      //play(this,this.xc,this.yc, this.type);
+      if(!blockInFront(this.map,this.dir,this.xc, this.yc)) {
+        this.blocked = true; 
+      }
+      else {
+        this.blocked = false;
+      }
+    }
+    if(this.dir == 2 && !this.blocked) {
+      this.y = this.y-this.speed;
+    }
+    if(this.dir == 3 && !this.blocked) {
+      this.y = this.y+this.speed;
+    }
+    if(this.dir == 1 && !this.blocked) {
+      this.x = this.x-this.speed;
+    }
+    if(this.dir == 0 && !this.blocked) {
+      this.x = this.x+this.speed;
+    }
+  }
+  
+  get_loca() {
+    return [this.xc,this.yc];
+  }
+  
+  play() {
+  }
+  isBlocked() {
+    return this.blocked;
+  }
+  
+  set_dir(dir) {
+    this.dir = dir;
+  }
+  
+  get_dir() {
+    return this.dir;
+  }
+  
+  set_speed(speed) {
+    this.speed = speed;
+  }
+  
+  get_x() {
+    return this.x;
+  }
+  
+  get_y() {
+    return this.y;
+  }
+}
---
+++ Pac_the_Man/pacman_new.js
@@ -0,0 +1,136 @@
+function updatePos(x,y,h,w) {
+  xMid = x / (w/20);
+  yMid = y / (h/20);
+  
+  return [xMid,yMid];
+}
+
+class PacMan{
+  constructor(start_x, start_y,hi,wd,maze) {
+    this.xc = start_x;
+    this.yc = start_y;
+    this.h = hi;
+    this.w = wd;
+    this.x = this.xc * this.h/20;
+    this.y = this.yc * this.w/20;
+    this.looking = 5;
+    this.speed = 2;
+    this.map = maze;
+  }
+  
+  changeDir(dir) {
+    if((((this.y+(this.h/40)) % (this.h/20)) == 20) && ((((this.x+(this.w/40)) % (this.w/20)) == 20))) {
+      var pos = updatePos(this.x,this.y,this.h,this.w);
+      this.xc = pos[0];
+      this.yc = pos[1];
+      //print(updatePos(this.x,this.y,this.h,this.w));
+    }
+    if(dir == 'SPACE') {
+      this.looking = 5;
+    }
+    if(dir == 'UP' || dir == 'DOWN') {
+      if(((this.x+(this.w/40)) % (this.w/20)) == 20) {
+        if(dir == 'UP') {
+          if(blockInFront(this.map,2,this.xc,this.yc)) {
+            this.looking = 2;
+          }
+        }
+        else {
+          if(blockInFront(this.map,3,this.xc,this.yc)) {
+            this.looking = 3;
+          }
+        }
+      }
+    }
+    else if(dir == "RIGHT" || dir == "LEFT") {
+      if(((this.y+(this.h/40)) % (this.h/20)) == 20) {
+        if(dir == "RIGHT") {
+          if(blockInFront(this.map,0,this.xc,this.yc)) {
+            this.looking = 0;
+          }
+        }
+        else {
+          if(blockInFront(this.map,1,this.xc,this.yc)) {
+            this.looking = 1;
+          }
+        }
+      }
+    }
+  }
+  
+  move() {
+    if(this.looking == 0 && blockInFront(this.map,this.looking,this.xc,this.yc)) {// right
+      if((this.x <= width && this.y <= height && this.x >=0 && this.y >= 0)) {
+        this.x += this.speed;
+        this.looking = 0;
+      }
+    }
+    if(this.looking == 1 && blockInFront(this.map,this.looking,this.xc,this.yc)) {// left
+      if((this.x <= width && this.y <= height && this.x >=0 && this.y >= 0)) {
+        this.x += -1 *this.speed;
+        this.looking = 1;
+      }
+    }
+    if(this.looking == 2 && blockInFront(this.map,this.looking,this.xc,this.yc)) { // up
+      if((this.x <= width && this.y <= height && this.x >=0 && this.y >= 0)) {
+        this.y += this.speed*-1;
+        this.looking = 2;
+      }
+    }
+    if(this.looking == 3 && blockInFront(this.map,this.looking,this.xc,this.yc)) {// down
+      if((this.x <= width && this.y <= height && this.x >=0 && this.y >= 0)) {
+        this.y += this.speed;
+        this.looking = 3;
+      }
+    }
+  }
+  
+  play() {
+    fill(255, 0, 0);
+    textSize(40);
+    text([this.xc,this.yc],100,33);
+
+  }
+  
+  get_pacCords() {
+    return [this.xc,this.yc];
+  }
+  
+  get_dir() {
+    return this.looking;
+  }
+  
+  get_x() {
+    return this.x;
+  }
+  
+  get_y() {
+    return this.y;
+  }
+  
+  set_speed(speed) {
+    this.speed = speed;
+  }
+  
+  drawPac() {
+    noStroke();
+    ellipseMode(CORNER);
+    fill(255,255,0);
+    ellipse(this.x,this.y,this.h/20,this.w/20);
+    fill(255,255,255);
+    var ww = this.w/20;
+    var hh = this.h/20;
+    if(this.looking == 0) { // right
+      triangle(this.x+(ww/2), this.y+(hh/2), this.x+(ww), this.y+(hh), this.x+(ww),this.y);
+    }
+    else if (this.looking == 1) { // left
+      triangle(this.x+(ww/2), this.y+(hh/2), this.x, this.y, this.x,this.y+(hh));
+    }
+    else if (this.looking == 2) { // up
+      triangle(this.x+(ww/2), this.y+(hh/2), this.x, this.y, this.x+(hh),this.y);
+    }
+    else if (this.looking == 3) { //down
+      triangle(this.x+(ww/2), this.y+(hh/2), this.x, this.y+(hh), this.x+(ww),this.y+(hh));
+    }
+  }
+}
